205071d7e28fea46aae604584d9b29f467ee7c57
Анастасия <nastena.andreevna.99@gmail.com>
2017-11-29 02:17:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,29 +3,35 @@
 #include <future>
 #include <thread>
 #include <curl/curl.h>
-void getCode(std::future<char*> & future)
+
+void GetResponse(std::future<char*> &fut)
 {
-    CURL * curl;
-    long code = 0;
-    CURLcode result;
-    char * fut = future.get();
+    CURL *curl;
+    CURLcode res;
+    char *url = fut.get();
     curl = curl_easy_init();
-    if (curl) 
-    {
-      curl_easy_setopt(curl, CURLOPT_URL, fut);
+    if (curl) {
+      curl_easy_setopt(curl, CURLOPT_URL, url);
       curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
     }
-    result = curl_easy_perform(curl);
-    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, & code);
-    std::cout << "Code of http : " << code << std::endl;
+    res = curl_easy_perform(curl);
+    long http_code = 0;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+    std::cout << "Response answer == " << http_code << '\n';
 }
+
 int main(int argc, char *argv[])
 {
-    char * url = argv[1];
-    std::promise<char*> promise;
-    std::future<char*> future = promise.get_future();
-    std::thread thread (getCode, std::ref(future));
-    promise.set_value(url);
-    thread.join();
+    char *url = argv[1];
+
+    std::promise<char*> prom;
+
+    std::future<char*> fut = prom.get_future();
+
+    std::thread th1 (GetResponse, std::ref(fut));
+
+    prom.set_value(url);
+
+    th1.join();
     return 0;
 }
